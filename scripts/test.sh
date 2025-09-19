3e961b40679f247d77afeb45e23c2f1890b835ef
qaxuDev <52445254+qaxuDev@users.noreply.github.com>
2025-09-19 20:41:57 +0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -29,7 +29,7 @@ python -m torch.distributed.launch \
     --nproc_per_node=$1 \
     --master_addr=localhost \
     --master_port=$2 \
-    inference_gate.py \
+    inference.py \
     --config=$config --labeled-id-path $labeled_id_path --unlabeled-id-path $unlabeled_id_path \
     --save-path $save_path --port $2 2>&1 | tee $save_path/$now.log
 
